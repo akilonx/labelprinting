f5720096bfa7b1f05ea22beffa6ceca77d2ea25d
Akilon Krishnan <Akilon.Krishnan@ascenderhcm.com>
2018-07-19 10:07:40 +0800

    Last page blank space fix

--- print.php
+++ print.php
@@ -26,9 +26,9 @@ $barcodeType = 'QRCODE';
 $promptPrint = false;
 
 
-
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
@@ -69,9 +69,50 @@ $pdf->SetFont('freeserif', '', 9);
 
 //start adding page
 $pdf->AddPage();
-        
-$sql = "SELECT * FROM orders where address_with_separator is not null and qrcode is not null";
+
+
+function addSpace($a, $pos){
+    global $itemLimit; 
+    global $pdf; 
+
+    $a = ($pos == 1) ? $a : $a + 1;
+
+    for ($i = $pos; $i <= $a; $i++) {
+
+        $y = $pdf->getY();
+
+        if ($i % 2 == 0) {
+            $pdf->Image('images/dash-black.png', 107, $y-1.5, 106, 3);
+            $border = array(
+                    'L' => array(
+                        'width' => 0.2, // careful, this is not px but the unit you declared
+                        'dash'  => 5,
+                        'color' => array(0, 0, 0)
+                    )
+                );
+            $pdf->writeHTMLCell(106, 74.2, '', '', '', $border, 1, 1, true, 'J', true);
+            $pdf->Ln(0);
+
+        } else {
+            /* $border = array(
+                'B' => array(
+                    'width' => 1, // careful, this is not px but the unit you declared
+                    'dash'  => 10,
+                    'color' => array(0, 0, 0)
+                )
+            ); */
+            $pdf->Image('images/dash-black-scissor.png', 0, $y-1.5, 104, 3);
+            $pdf->writeHTMLCell(106, 74.2, '', $y, '', $border = 0, 0, 1, true, 'J', true);
+        }
+
+    }
+}
+
+
+//Program start        
+$sql = "SELECT * FROM orders where address_with_separator is not null and qrcode is not null limit 13";
 $result = $conn->query($sql);
+$resultIndex = $result->num_rows;
 
 if ($result->num_rows > 0) {
     $i = 0; //setting loop index
@@ -87,61 +128,39 @@ if ($result->num_rows > 0) {
         
         $y = $pdf->getY();
 
-        if ($i <= $itemLimit) {
-            $html = '
-            <table width="100%" border="0" cellpadding="5">
-                <tr>
-                    <td align="center" width="75"> 
-                        <div style="height:100px"><br><br><br><br></div>
-                        <div><span style="text-align:center; font-size:30px; font-weight:bold;">&nbsp;&nbsp;'.($item['qrcode2']? "B": "").'</span></div>
-                    </td>
-                    <td align="left" width="270">
-                        <div>
-                            <table width="100%" style="padding-top:10px" cellspacing="0" border="0" cellpadding="0">
-                                <tr>
-                                    <td width="30%"><span style="text-align:left;font-size:12;font-weight:bold;">'.(($showMobile == 1) ? $item['mobile'] : '').'</span></td>
-                                    <td width="70%"><span style="text-align:right;font-size:12;font-weight:bold;">'.(($showAmount == 1) ? $item['cod'].' '.$item['amount'] : '').'</span></td>
-                                </tr>
-                            </table>
-                            <table width="100%" cellspacing="0" border="0" cellpadding="0">
-                                <tr>
-                                    <td><span style="padding-left:15px;font-size:12;line-height:1.7">'.(($showAddress == 1) ? $item['address'] : '').'</span></td>
-                                </tr>
-                            </table>
-                        </div>
-                    </td>
-                </tr>
-            </table>
-            <img src="images/dash.png">
-            ';
+    
+        $html = '
+        <table width="100%" border="0" cellpadding="5">
+            <tr>
+                <td align="center" width="75"> 
+                    <div style="height:100px"><br><br><br><br></div>
+                    <div><span style="text-align:center; font-size:30px; font-weight:bold;">&nbsp;&nbsp;'.($item['qrcode2']? "B": "").'</span></div>
+                </td>
+                <td align="left" width="270">
+                    <div>
+                        <table width="100%" style="padding-top:10px" cellspacing="0" border="0" cellpadding="0">
+                            <tr>
+                                <td width="30%"><span style="text-align:left;font-size:12;font-weight:bold;">'.(($showMobile == 1) ? $item['mobile'] : '').'</span></td>
+                                <td width="70%"><span style="text-align:right;font-size:12;font-weight:bold;">'.(($showAmount == 1) ? $item['cod'].' '.$item['amount'] : '').'</span></td>
+                            </tr>
+                        </table>
+                        <table width="100%" cellspacing="0" border="0" cellpadding="0">
+                            <tr>
+                                <td><span style="padding-left:15px;font-size:12;line-height:1.7">'.(($showAddress == 1) ? $item['address'] : '').'</span></td>
+                            </tr>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <img src="images/dash.png">
+        ';
 
-            if ($i % 2 == 0) {
-                $pdf->write2DBarcode(($item['qrcode2'] ? $item['qrcode2'] : $item['qrcode']), $barcodeType, 110.2, $y+6.5, 17, 17);
-            } else {
-                $pdf->write2DBarcode(($item['qrcode2'] ? $item['qrcode2'] : $item['qrcode']), $barcodeType, 4, $y+6.5, 17, 17);
-            }
-
+        if ($i % 2 == 0) {
+            $pdf->write2DBarcode(($item['qrcode2'] ? $item['qrcode2'] : $item['qrcode']), $barcodeType, 110.2, $y+6.5, 17, 17);
         } else {
-            $html = '';
+            $pdf->write2DBarcode(($item['qrcode2'] ? $item['qrcode2'] : $item['qrcode']), $barcodeType, 4, $y+6.5, 17, 17);
         }
-
-        
-        //prints content body
-        /*
-            $this->writeHTMLCell(
-            $width = 0, // width of the cell, not the input
-            $height = 0, // height of the cell..
-            $x,
-            $y,
-            $html = '', // your input.
-            $border = 0,
-            $ln = 0,
-            $fill = false,
-            $reseth = true,
-            $align = '',
-            $autopadding = true 
-        );
-        */
 
         if ($i % 2 == 0) {
             if ($i > 2) $pdf->Image('images/dash-black.png', 107, $y-1.5, 106, 3);
@@ -168,11 +187,19 @@ if ($result->num_rows > 0) {
         }
 
         //max page
-        if ($i == 8) {
+        
+        if ($i == $itemLimit) {
+            //max page
+            addSpace(8 - $itemLimit, 1);
             $i = 0;
             $pdf->AddPage();
         }
-        
+
+        if( --$resultIndex===0 ){
+            $pos = ($i % 2 == 0) ? 1 : 2;
+            addSpace(8 - $i, $pos);
+        }
+
     }
 }
 
